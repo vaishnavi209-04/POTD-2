09bd7003f86aa37c81ef5d5b0fd99f4671293ede
Vaishnavi Mishra <vaishnavimishra986@gmail.com>
2025-05-06 09:45:03 +0530

    Build Array from Permutation

--- MAY/6_1920/1920.java
+++ MAY/6_1920/1920.java
@@ -28,3 +28,23 @@ class Solution {
         }
     }
 }
+//Approach 3-O(n)
+//Maths-num=a+bq
+//new value->num/b
+//old value->num%b
+//here limit is 1 to n-1 so take b as n
+//SC-O(1)
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+            nums[i]=nums[i]+n*(nums[nums[i]]%n);//value has already changed so we are doing %n to get old value for calculation here
+        }
+        for(int i=0;i<n;i++)
+        {
+            nums[i]=nums[i]/n;
+        }
+        return nums;
+    }
+}
