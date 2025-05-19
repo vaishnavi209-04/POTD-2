4c17696f55d6650f2cbb23d3b6ba2b0a974d6404
Vaishnavi Mishra <vaishnavimishra986@gmail.com>
2025-05-19 18:40:52 +0530

    Type of Triangle

---
+++ MAY/19_3024/3024.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String triangleType(int[] nums) {
+        if(nums[0]==nums[1] && nums[1]==nums[2])
+        return "equilateral";
+        else if((nums[0]==nums[1] && nums[0]+nums[1]>nums[2])||(nums[1]==nums[2] && nums[1]+nums[2]>nums[0])||(nums[0]==nums[2] && nums[0]+nums[2]>nums[1]))
+        return "isosceles";
+        else if(nums[0]+nums[1]>nums[2] && nums[0]+nums[2]>nums[1] && nums[1]+nums[2]>nums[0])
+        return "scalene";
+        else
+        return "none";
+
+        
+    }
+}
