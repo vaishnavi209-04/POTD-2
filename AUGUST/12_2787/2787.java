21744dea6aca1188bc399b1a670a81fef92e03cc
Vaishnavi Mishra <vaishnavimishra986@gmail.com>
2025-08-12 14:47:03 +0530

    Ways to Express an Integer as Sum of Powers

--- AUGUST/12_2787/2787.java
+++ AUGUST/12_2787/2787.java
@@ -25,3 +25,22 @@ class Solution {
         return dp[n][num]=(take+skip)% mod;
     }
 }
+//Approach 2-bottom up dp
+//T.C-O(n^2)
+class Solution {
+    int mod=1_000_000_007;
+    public int numberOfWays(int n, int x) {
+       long[] dp=new long[n+1];//no of ways to have sum s
+       dp[0]=1;//sum 0 by taking nothing
+       for(int i=1;(int)Math.pow(i,x)<=n;i++)
+       {
+        int num=(int)Math.pow(i,x);
+        //if no of ways to make S(6)=2 and we have num=4 currently then no of ways to make S(10)=S(10-4)
+        for(int s=n;s>=num;s--)
+        {
+            dp[s]+=dp[s-num];
+        }
+       }
+       return (int)(dp[n] % mod);
+    }
+}
