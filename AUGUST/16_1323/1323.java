cd7d61f09c5db2de354d120b813170ce8a0e97a7
Vaishnavi Mishra <vaishnavimishra986@gmail.com>
2025-08-16 13:41:02 +0530

    Maximum 69 Number

---
+++ AUGUST/16_1323/1323.java
@@ -0,0 +1,16 @@
+//Approach 1-O(n)
+class Solution {
+    public int maximum69Number (int num) {
+        char[] arr=Integer.toString(num).toCharArray();
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]=='6')
+            {
+            arr[i]='9';
+            break;
+            }
+        }
+        return Integer.parseInt(new String(arr));
+    }
+}
