24b50535fd5c865243269008a83d331552adc7cf
Vaishnavi Mishra <vaishnavimishra986@gmail.com>
2025-11-18 15:41:07 +0530

    1-bit and 2-bit Characters

---
+++ NOVEMBER/18_717/717.java
@@ -0,0 +1,15 @@
+//Approach 1-Brute Force-O(N)
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        int n=bits.length;
+        int i=0;
+        while(i<n-1)
+        {
+            if(bits[i]==0)
+            i++;
+            else
+            i+=2;
+        }
+        return i==n-1;
+    }
+}
