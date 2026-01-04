4f4cd27461f96c3cc82a8100102a35aa14b7abdc
Vaishnavi Mishra <vaishnavimishra986@gmail.com>
2026-01-04 17:16:26 +0530

    Four Divisors

---
+++ 2026/JANUARY/4_1390/1390.java
@@ -0,0 +1,34 @@
+//Approach 1-O(n*(root max num))
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int res=0;
+        for(int num:nums)
+        {
+            int count=0;
+            int sum=0;
+            for(int i=1;i*i<=num;i++)
+            {
+                if(count>4)
+                break;
+                if(num%i==0)
+                {
+                    int d1=i;
+                    int d2=num/i;
+                    if(d1==d2)
+                    {
+                        count++;
+                        sum+=d1;
+                    }
+                    else
+                    {
+                        count+=2;
+                        sum+=d1+d2;
+                    }
+                }
+            }
+            if(count==4)
+            res+=sum;
+        }
+        return res;
+    }
+}
