9c2b7e9ef0406f8a631558931a22c199f6347e73
Vaishnavi Mishra <vaishnavimishra986@gmail.com>
2026-01-24 14:48:43 +0530

    Minimize Maximum Pair Sum in Array

---
+++ 2026/JANUARY/24_1877/1877.java
@@ -0,0 +1,13 @@
+//O(n log n)
+class Solution {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int max=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+           max=Math.max(max,nums[i]+nums[n-i-1]);
+        }
+        return max;
+    }
+}
