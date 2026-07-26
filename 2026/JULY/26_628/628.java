81898af6d1fc1566c166450a375b3908c767e35e
Vaishnavi Mishra <vaishnavimishra986@gmail.com>
2026-07-26 14:45:28 +0530

    Maximum Product of Three Numbers

---
+++ 2026/JULY/26_628/628.java
@@ -0,0 +1,11 @@
+//Approach 1-Sorting-O(n log n)
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        int a=nums[n-1]*nums[n-2]*nums[n-3];
+        int b=nums[0]*nums[1]*nums[n-1];
+
+        return Math.max(a,b);
+    }
+}
