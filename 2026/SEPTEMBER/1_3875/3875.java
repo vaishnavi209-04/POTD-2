cb9d9337bd8bf85bd02abdcc03e014cced4c98e8
Vaishnavi Mishra <vaishnavimishra986@gmail.com>
2026-09-02 14:08:07 +0530

    Construct Uniform Parity Array I

---
+++ 2026/SEPTEMBER/1_3875/3875.java
@@ -0,0 +1,8 @@
+//Approach 1:O(1)
+//if nums have all even or all odd then just copy to make another nums
+//if there is even and odd both in nums then we can make other nums odd always because even-odd=odd 
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
