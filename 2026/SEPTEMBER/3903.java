4a5eddb09edd0ab1625d25686f2c147982b734dc
Vaishnavi Mishra <vaishnavimishra986@gmail.com>
2026-09-04 14:32:08 +0530

    Smallest Stable Index I

---
+++ 2026/SEPTEMBER/3903.java
@@ -0,0 +1,27 @@
+//Approach 1:O(n)
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n=nums.length;
+        int[] max=new int[n];
+        int[] min=new int[n];
+        max[0]=nums[0];
+        min[n-1]=nums[n-1];
+
+        for(int i=1;i<n;i++)
+        {
+            max[i]=Math.max(nums[i],max[i-1]);
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            min[i]=Math.min(nums[i],min[i+1]);
+        }
+        int res=-1;
+        for(int i=0;i<n;i++)
+        {
+            int curr=max[i]-min[i];
+            if(curr<=k)
+            return i;
+        }
+        return res;
+    }
+}
